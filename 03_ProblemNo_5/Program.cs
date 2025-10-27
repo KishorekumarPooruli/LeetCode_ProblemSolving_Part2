81ddb799592d4e87172c3e8d92668870229877c2
kishorekumar.pooruli@outlook.com <kishorekumar.pooruli@outlook.com>
2025-10-27 17:28:24 +0000

    update

---
+++ 03_ProblemNo_5/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Net;
+
+namespace _03_ProblemNo_5
+{
+    /// <summary>
+    /// https://leetcode.com/problems/longest-palindromic-substring
+    /// </summary>
+    internal class RunnerModule
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string result = solution.LongestPalindrome("ccc");
+        }
+    }
+
+    public class Solution
+    {
+        public string? LongestPalindrome(string s)
+        {
+            int leftPointerIndex = 0;
+            int rightPointerIndex = 0;
+            int centrePointerIndex = 0;
+            List<string> outputs = new List<string>();
+
+            for(int i = centrePointerIndex; i < s.Length; i++)
+            {
+                char centerValue = s[i];
+                leftPointerIndex = i;
+                rightPointerIndex = i;
+                while (leftPointerIndex < s.Length || rightPointerIndex < s.Length)
+                {
+                    char leftValue = leftPointerIndex >= 0 && leftPointerIndex < s.Length ? s[leftPointerIndex] :  ' ';
+                    char rightValue = rightPointerIndex >= 0 && rightPointerIndex < s.Length ? s[rightPointerIndex] : ' ';
+
+                    if (leftValue.Equals(' ') && rightValue.Equals(' '))
+                    {
+                        break;
+                    }
+
+                    if (centerValue.Equals(leftValue) && centerValue.Equals(rightValue))
+                    {
+                        outputs.Add(centerValue.ToString()); 
+                    }
+                    else if (centerValue.Equals(leftValue))
+                    {
+                        outputs.Add($"{centerValue}{leftValue}");
+                    }
+                    else if (centerValue.Equals(rightValue))
+                    {
+                        outputs.Add($"{centerValue}{rightValue}");
+                    }
+                    else if (leftValue.Equals(rightValue))
+                    {
+                        outputs.Add($"{leftValue}{centerValue}{rightValue}");
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+
+                    leftPointerIndex = leftPointerIndex - 1;
+                    rightPointerIndex = rightPointerIndex + 1;
+                }
+
+            }
+
+            return outputs.OrderByDescending(s => s.Length).FirstOrDefault();
+        }
+    }
+
+
+}
---
+++ 04_ProblemNo_643/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+
+namespace _04_ProblemNo_643
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-average-subarray-i/description
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] { 1, 12, -5, -6, 50, 3 };
+            Solution solution = new Solution();
+            double result = solution.FindMaxAverage(input, 1);
+            //// 1,12,-5,-6,50,3
+            ////  1,12,-5,-6
+            //// 12,-5,-6,50
+            //// -5,-6,50,3
+        }
+    }
+
+    public class Solution
+    {
+        public double FindMaxAverage(int[] inputArray, int k)
+        {
+            double windowSum = default(double);
+            double avg = default(double);
+            int previousStartingIndex = 0;
+            int startingWindowIndex = 0;
+            int endingWindowIndex = k;
+
+            for (int start = 0; start < inputArray.Length; start++)
+            {
+                if (start + k <= inputArray.Length) //// Checking if we have a Window
+                {
+                    if (start == 0)
+                    {
+                        //// First Iteration Calculate SUM of all the Window
+                        for (int i = startingWindowIndex; i < endingWindowIndex; i++)
+                        {
+                            windowSum += inputArray[i];
+                        }
+
+                        previousStartingIndex = startingWindowIndex; //// Tracking Element to be Deleted in Next Iteration
+                        endingWindowIndex--;  //// Assigning Last Index of the current Sliding Window
+                    }
+                    else
+                    {
+                        //// Consecutive Iteration Just Add and Subtract
+                        windowSum += inputArray[endingWindowIndex + 1] - inputArray[previousStartingIndex];
+                        previousStartingIndex++;
+                        endingWindowIndex++;
+                    }
+
+                    var result = windowSum / (double)k;
+                    avg = avg != 0 ? Math.Max(avg, result) : result;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return avg;
+        }
+    }
+}
