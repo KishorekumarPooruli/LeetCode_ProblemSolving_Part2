64e166a9e813f0db960ad5bbc1293b18fefdb635
kishorekumar.pooruli@outlook.com <kishorekumar.pooruli@outlook.com>
2025-11-04 10:17:50 +0000

    Problem Solving-1984,2269

---
+++ 05_ProblemNo_1984/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _05_ProblemNo_1984
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] input = new int[] { 87063, 61094, 44530, 21297, 95857, 93551, 9918 };
+            solution.MinimumDifference(input, 6);
+        }
+    }
+
+    public class Solution
+    {
+        public int MinimumDifference(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int result = int.MaxValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int[] subsetWindow = nums.Skip(i).Take(k).ToArray();
+
+                if (subsetWindow.Length < k)
+                {
+                    break;
+                }
+
+                int maxNo = subsetWindow.Max();
+                int minNo = subsetWindow.Min();
+                int diff = maxNo - minNo;
+                result = Math.Min(result, diff);
+            }
+
+            return result == int.MaxValue ? 0 : result;
+        }
+    }
+}
---
+++ 06_ProblemNo_2269/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+
+namespace _06_ProblemNo_2269
+{
+    /// <summary>
+    /// https://leetcode.com/problems/find-the-k-beauty-of-a-number/description 
+    //// Fixed Size Sliding Window  
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int kBeauty = solution.DivisorSubstrings(430043, 2);
+        }
+    }
+
+    public class Solution
+    {
+        public int DivisorSubstrings(int num, int k)
+        {
+            int kBeauty = 0;
+            string input = Convert.ToString(num);
+
+            for (int i = 0; i < input.Length; i++) 
+            {
+                char[] window = input.Skip(i).Take(k).ToArray();
+                if (window.Length == k)
+                {
+                    int divisor = int.Parse(new string(window));
+                    int remainder = divisor != 0 ? num % divisor : 1; 
+                    if (remainder == 0)
+                    {
+                        kBeauty++;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            return kBeauty;
+        }
+    }
+}
