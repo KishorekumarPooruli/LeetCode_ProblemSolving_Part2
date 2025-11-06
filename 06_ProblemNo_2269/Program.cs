956b73c7267c245a7cc9b827dd7764997edd3085
kishorekumar.pooruli@outlook.com <kishorekumar.pooruli@outlook.com>
2025-11-06 08:26:48 +0000

    time complexity and alternative code updated

--- 06_ProblemNo_2269/Program.cs
+++ 06_ProblemNo_2269/Program.cs
@@ -5,13 +5,14 @@ namespace _06_ProblemNo_2269
     /// <summary>
     /// https://leetcode.com/problems/find-the-k-beauty-of-a-number/description 
     //// Fixed Size Sliding Window  
+    //// Time Complexity for Both Programs are O(n × k), O(n)---FAIR
     /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
             Solution solution = new Solution();
-            int kBeauty = solution.DivisorSubstrings(430043, 2);
+            int kBeauty = solution.DivisorSubstringsAIGenerated(240, 2);
         }
     }
 
@@ -24,6 +25,7 @@ namespace _06_ProblemNo_2269
 
             for (int i = 0; i < input.Length; i++) 
             {
+                //// substring gets converted to Char[] and then to string to int
                 char[] window = input.Skip(i).Take(k).ToArray();
                 if (window.Length == k)
                 {
@@ -42,5 +44,25 @@ namespace _06_ProblemNo_2269
             
             return kBeauty;
         }
+
+        public int DivisorSubstringsAIGenerated(int num, int k)
+        {
+            int kBeauty = 0;
+            string input = Convert.ToString(num);
+
+            for (int i = 0; i <= input.Length - k; i++)
+            {
+                string window = input.Substring(i, k); //// i same as Skip (i). k same as Take(k)
+                int divisor = int.Parse(window);
+
+                if (divisor != 0 && num % divisor == 0)
+                {
+                    kBeauty++;
+                }
+            }
+
+            return kBeauty;
+        }
+
     }
 }
