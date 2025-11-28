24ca86c6845bf53a31d3944d5524ce6eeb35dd01
kishorekumar.pooruli@outlook.com <kishorekumar.pooruli@outlook.com>
2025-11-28 11:10:48 +0000

    update

---
+++ 15_ProblemNo_80/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _15_ProblemNo_80
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] input = new int[] { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+            solution.RemoveDuplicates(input);
+        }
+    }
+
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int rightPointer = 1;
+            for (int leftPointer = 0; leftPointer < nums.Length; leftPointer++)
+            {
+                if(rightPointer < nums.Length)
+                {
+                    if (nums[leftPointer] == nums[rightPointer])
+                    {
+                        rightPointer++;
+                    }
+                    else
+                    {
+                        int temp = nums[leftPointer];
+                        nums[leftPointer] = nums[rightPointer];
+                        nums[rightPointer] = temp;
+                        rightPointer++;
+                    }
+                }                
+            }
+
+            return nums.Length;
+        }
+    }
+}
---
+++ 16_ProblemNo_1732/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _16_ProblemNo_1732
+{
+    /// <summary>
+    /// https://leetcode.com/problems/find-the-highest-altitude/description
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.LargestAltitudeOptimal(new int[] { -4, -3, -2, -1, 4, 3, 2 });
+        }
+    }
+
+    public class Solution
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            //// TRADITIONAL PREFIX SUM but uses extra space
+            int maxSumValueTrack = 0;
+            int[] resultArray = new int[gain.Length + 1];
+            resultArray[gain.Length] = 0;
+            for (int i = 0; i < gain.Length; i++)
+            {
+                maxSumValueTrack = maxSumValueTrack + gain[i];
+                resultArray[i] = maxSumValueTrack + gain[i];
+                if (resultArray[i] > maxSumValueTrack)
+                {
+                    maxSumValueTrack = resultArray[i];
+                }
+            }
+
+            return maxSumValueTrack;
+        }
+
+        public int LargestAltitudeOptimal(int[] gain)
+        {
+            int maxSumValueTrack = 0;
+            for (int i = 0; i < gain.Length; i++)
+            {
+                if (i > 0)
+                {
+                    gain[i] = gain[i] + gain[i - 1];   // in-place prefix sum
+                }
+
+                if (gain[i] > maxSumValueTrack)
+                {
+                    maxSumValueTrack = gain[i];
+                }
+            }
+
+            return maxSumValueTrack;
+        }
+    }
+}
