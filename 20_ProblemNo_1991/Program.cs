ccc9fba68a97172e7c1167ca3e2eeefeb05be139
kishorekumar.pooruli@outlook.com <kishorekumar.pooruli@outlook.com>
2025-12-19 08:34:25 +0000

    update

---
+++ 20_ProblemNo_1991/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _20_ProblemNo_1991
+{
+    /// <summary>
+    /// TIME AND SPACE: O(N)
+    /// https://leetcode.com/problems/find-the-middle-index-in-array
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int result = solution.FindMiddleIndex(new int[] { 2, 5 });
+        }
+    }
+
+    public class Solution
+    {
+        public int FindMiddleIndex(int[] nums)
+        {
+            int[] prefixArray = this.BuildPrefixSum(nums);
+            int totalSum = prefixArray[nums.Length - 1];
+
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                int leftSum = 0;
+                if (i - 1 >= 0)
+                {
+                    leftSum = prefixArray[i - 1];
+                }
+                int rightSum = totalSum - leftSum - nums[i];
+
+                if (leftSum == rightSum)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private int[] BuildPrefixSum(int[] nums)
+        {
+            int[] intPrefixArray = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                if (i - 1 >= 0)
+                {
+                    intPrefixArray[i] = nums[i] + intPrefixArray[i -1];
+                }
+                else
+                {
+                    intPrefixArray[i] = nums[i];
+                }
+            }
+
+            return intPrefixArray;
+        }
+
+    }
+}
---
+++ 21_ProblemNo_3432/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _21_ProblemNo_3432
+{
+    /// <summary>
+    /// https://leetcode.com/problems/count-partitions-with-even-sum-difference
+    /// TIME AND SPACE COMPLEXITY: O(n)
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.CountPartitions(new int[] { 2, 4, 6, 8 });
+        }
+    }
+
+    public class Solution
+    {
+        public int CountPartitions(int[] nums)
+        {
+            int resultEvenSum = 0;
+            int[] prefixArray = this.BuildPrefixSum(nums);
+            for (int i = 0; i < nums.Length - 1; i++) 
+            {
+                int currentIndexPrefix = prefixArray[i]; //// CONTAINS PREFIX OF LEFT ELEMENTS
+                int remaningPrefixRightSide = prefixArray[nums.Length - 1] - currentIndexPrefix;
+                int difference = currentIndexPrefix - remaningPrefixRightSide;
+                if (difference % 2 == 0) 
+                {
+                    resultEvenSum++;
+                }
+            }
+
+            return resultEvenSum;
+        }
+
+        private int[] BuildPrefixSum(int[] nums)
+        {
+            int[] intPrefixArray = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i - 1 >= 0)
+                {
+                    intPrefixArray[i] = nums[i] + intPrefixArray[i - 1];
+                }
+                else
+                {
+                    intPrefixArray[i] = nums[i];
+                }
+            }
+
+            return intPrefixArray;
+        }
+    }
+}
